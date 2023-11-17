2500c6ad81f1dff169c9493690385a6d6ad26c99
as2872 <as2872@A45629>
2023-11-17 16:51:08 +0000

    mvp

--- Program.cs
+++ Program.cs
@@ -1,21 +1,67 @@
-namespace CodeFive
+using System;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+using System.IO;
+using System.Data;
+
+namespace VendorApplication
 {
-    internal static class Program
+    static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
 
-            // Commit.
+    public class MainForm : Form
+    {
+        private Button btnUploadPDF;
+        private TextBox txtFilePath;
+        private SaveFileDialog saveFileDialog;
 
+        public MainForm()
+        {
+            btnUploadPDF = new Button { Text = "Upload PDF" };
+            btnUploadPDF.Click += BtnUploadPDF_Click;
 
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            txtFilePath = new TextBox { Width = 400 };
+            saveFileDialog = new SaveFileDialog { Filter = "PDF files (*.pdf)|*.pdf" };
+
+            Controls.Add(btnUploadPDF);
+            Controls.Add(txtFilePath);
+
+            btnUploadPDF.Location = new Point(10, 10);
+            txtFilePath.Location = new Point(10, 50);
+        }
+
+        private void BtnUploadPDF_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = saveFileDialog.FileName;
+                SaveDocumentPathToDatabase(saveFileDialog.FileName);
+            }
+        }
+
+        private void SaveDocumentPathToDatabase(string filePath)
+        {
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\VendorApplicationDB.mdf;Integrated Security=True";
+            string query = "INSERT INTO Documents (DocumentName, DocumentPath) VALUES (@name, @path)";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@name", Path.GetFileName(filePath));
+                    cmd.Parameters.AddWithValue("@path", filePath);
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
