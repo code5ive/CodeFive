6023cd5348ce91ca1ddc813b118f6e4fbcff5d35
Akbar Sharifov <akbarjon.sharifov01@gmail.com>
2023-12-07 04:37:08 +0000

    button to open a pdf

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,16 @@ namespace CodeFive
         public Form1()
         {
             InitializeComponent();
+            
+
+            // Button to open a PDF
+            Button openPdfButton = new Button();
+            openPdfButton.Text = "Open PDF";
+            openPdfButton.Location = new System.Drawing.Point(133, 442); // Adjust position as needed
+            openPdfButton.Size = new System.Drawing.Size(280, 160); // Adjust size as needed
+            openPdfButton.Click += new EventHandler(OpenPdfButton_Click);
+            this.Controls.Add(openPdfButton);
+
         }
 
         private void button1_Click(object sender, EventArgs e)
